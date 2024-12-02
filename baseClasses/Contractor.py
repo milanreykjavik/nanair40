78641b115e024fe2f0bc93a9e689c38bd4f8bbae
cryp70m4n <cryp70m4n@gmail.com>
2024-12-02 10:29:23 +0000

    Small update to base classes

---
+++ baseClasses/Contractor.py
@@ -0,0 +1,15 @@
+from typing import Type
+
+
+class Contractor:
+    def __init__(self) -> None:
+        self.id: int = 0
+        self.name: str = ""
+        self.phoneNumber: str = ""
+        self.openingHours: str = "" # might change to date
+        self.country: int = -1
+
+
+    def addContractor(self, employee: Type[Contractor]) -> bool:
+        # call data layer here
+        return True
--- baseClasses/Employee.py
+++ baseClasses/Employee.py
@@ -3,14 +3,14 @@ from typing import Type
 
 class Employee:
     def __init__(self) -> None:
-        self.id = -1
-        self.kinnetala = -1
-        self.name = ""
-        self.phone = ""
-        self.honePhone = ""
-        self.address = ""
-        self.email = ""
-        self.country = -1 # country will be list of countries as dict
+        self.id: int = -1
+        self.kinnetala: str = ""
+        self.name: str = ""
+        self.phone: str = ""
+        self.honePhone: str = ""
+        self.address: str = ""
+        self.email: str = ""
+        self.country: int = -1 # country will be list of countries as dict
 
 
     def addEmployee(self, employee: Type[Employee]) -> bool:
