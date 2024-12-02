3994ba5bd9e91b40eba2c5acc1aaa8396e977a22
cryp70m4n <cryp70m4n@gmail.com>
2024-12-02 11:42:32 +0000

    Small baseUI edit

--- ui/baseUI.py
+++ ui/baseUI.py
@@ -1,52 +1,49 @@
-def getOptions(options: list) -> str:
-    returnStr = '\n'
+class BaseUI:
+    @staticmethod
+    def getOptions(options: list) -> str:
+        returnStr = '\n'
 
-    for option in options:
-        firstLetter = option[0]
-        returnStr += f'{"":<7}[{firstLetter}]{option[1:]}\n'
+        for option in options:
+            firstLetter = option[0]
+            returnStr += f'{"":<7}[{firstLetter}]{option[1:]}\n'
 
-    return returnStr
+        return returnStr
 
-def getHeader()->str:
-    return f'''
-===============================================================================
-                                NaN Air
-===============================================================================
+
+    @staticmethod
+    def getHeader()->str:
+        return f'''
+    ===============================================================================
+                                    NaN Air
+    ===============================================================================
           
-'''
+    '''
 
-def getFooter(inputOption: str)->str:
-    return f'''
--------------------------------------------------------------------------------
-    [Q]uit   [B]ack
+    @staticmethod
+    def getFooter(inputOption: str)->str:
+        return f'''
+    -------------------------------------------------------------------------------
+        [Q]uit   [B]ack
 
-{inputOption}:
-'''
+    {inputOption}:
+    '''
 
+    def printBaseMenu(self, name: str, options: list, inputOption: str) -> None:
+        baseMenu = ''
+        baseMenu += self.getHeader()
+        baseMenu += f'''       {name}
+            -----------------'''
+        baseMenu += self.getOptions(options)
+        baseMenu += self.getFooter(inputOption)
 
+        print(baseMenu)
 
+    @staticmethod
+    def takeInput(possibilites: list) -> int:
+        inp = input()
 
-def printBaseMenu(name: str, options: list, inputOption: str) -> None:
-    baseMenu = ''
-    baseMenu += getHeader()
-    baseMenu += f'''       {name}
-        -----------------'''
-    baseMenu += getOptions(options)
-    baseMenu += getFooter(inputOption)
-
-    print(baseMenu)
-
-
-
-
-printBaseMenu('Manager menu', ['Add employee', 'Edit employee', 'List employees'])
-
-
-def takeInput(possibilites: list) -> int:
-    inp = input()
-
-    # you need to check if option si valid
-    # return -1 if option is invalid and let user choose again
-    return 0 # I want you to return index (number) of option user choose from the list
+        # you need to check if option si valid
+        # return -1 if option is invalid and let user choose again
+        return 0 # I want you to return index (number) of option user choose from the list
 
 
