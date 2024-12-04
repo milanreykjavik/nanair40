133a1a3ac7d21f974b457daa2372b9a3dbb26fae
cryp70m4n <cryp70m4n@gmail.com>
2024-12-04 13:24:26 +0000

    TODO lists and logic wrapper fix

--- logic/logicWrapper.py
+++ logic/logicWrapper.py
@@ -6,9 +6,8 @@ from employeeHandler import EmployeeHandler
 from baseClasses.Employee import Employee
 
 class Logic_Wrapper:
-    def __init__(self, employee_handler: EmployeeHandler, search: Search) -> None:
+    def __init__(self, employee_handler: EmployeeHandler) -> None:
         self.employee_handler = employee_handler
-        self.search = search
 
     def addEmployee(self, employee: Employee) -> bool:
         return self.employee_handler.addEmployee(employee)
